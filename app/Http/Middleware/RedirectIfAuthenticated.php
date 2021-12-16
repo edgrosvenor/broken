f9d75ad2389e857df2d67e53e8196edef7ab75c9
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2021-12-15 19:16:42 -0600

    Found it

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -12,12 +12,10 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @param  string|null  ...$guards
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next, ...$guards)
+    public function handle(Request $request, Closure $next, ...$guards): \Symfony\Component\HttpFoundation\Response
     {
         $guards = empty($guards) ? [null] : $guards;
 
