630ada39d753fde938247c58d9871527449e1e7a
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2021-12-15 19:15:37 -0600

    Just thought of something

--- app/Models/Child.php
+++ app/Models/Child.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Child extends Model
 {
@@ -13,4 +14,11 @@ class Child extends Model
     {
         return $this->belongsTo(Dad::class);
     }
+
+    public function toys(): HasMany
+    {
+        return $this->hasMany(Toy::class)->where('broken_at', null);
+    }
+
+
 }
